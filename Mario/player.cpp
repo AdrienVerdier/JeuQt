bfcd47615a0562bab0ab3e2e138f9319edadcb94
PierreSavary1 <pierre.savary1@gmail.com>
2020-05-06 09:37:07 +0200

    jump

--- Mario/player.cpp
+++ Mario/player.cpp
@@ -2,6 +2,16 @@
 #include <QGraphicsScene>
 #include <QKeyEvent>
 
+int Player::get_debut_saut() const
+{
+    return this->debut_saut;
+}
+
+void Player::set_debut_saut(int debut_saut)
+{
+    this->debut_saut = debut_saut;
+}
+
 Player::Player(QGraphicsItem *parent): QGraphicsRectItem(parent){
 
 }
@@ -16,4 +26,33 @@ void Player::keyPressEvent(QKeyEvent *event){
         if (pos().x() + 100 < 2000)
         setPos(x()+10,y());
     }
+    else if (event->key() == Qt::Key_Up){
+
+        set_debut_saut(pos().y());
+        timer = new QTimer(this);
+        QObject::connect(timer,SIGNAL(timeout()),this,SLOT(jump()));
+        timer->start(50);
+    }
+    else if (event->key() == Qt::Key_Down){
+        setPos(x(),y()+10);
+    }
+}
+
+void Player::jump(){
+    if (pos().y() > 300)
+        setPos(x(),y()-10);
+    else
+    {
+        timer->stop();
+        timer = new QTimer(this);
+        QObject::connect(timer,SIGNAL(timeout()),this,SLOT(descente()));
+        timer->start(50);
+    }
+}
+
+void Player::descente(){
+    if (pos().y() < 500)
+        setPos(x(),y()+10);
+    else
+        timer->stop();
 }
--- Mario/player.h
+++ Mario/player.h
@@ -4,13 +4,22 @@
 #include <QGraphicsRectItem>
 #include <QObject>
 #include <QGraphicsItem>
+#include <QTimer>
 
 class Player:public QObject, public QGraphicsRectItem{
     Q_OBJECT
 public:
+
+    int get_debut_saut() const;
+    void set_debut_saut(int debut_saut);
     Player(QGraphicsItem * parent=0);
     void keyPressEvent(QKeyEvent * event);
+private :
+    int debut_saut;
+    QTimer *timer;
 public slots:
+    void jump();
+    void descente();
 };
 
 #endif // PLAYER_H
