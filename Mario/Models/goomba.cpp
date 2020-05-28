a6fb8ea4a1125b0cc1457eb4b2204591002f48d0
Yoann DUPAS <yoann.dupas@outlook.fr>
2020-05-28 16:08:36 +0200

    update goomba.cpp

--- Mario/Models/goomba.cpp
+++ Mario/Models/goomba.cpp
@@ -7,10 +7,19 @@ Goomba::Goomba()
 
 void Goomba::collision(Entity *entity, int position)
 {
-
+    if(position == 1){
+        move_to_left = true;
+        move_to_right = false;
+    }
+    if(position == 2){
+        move_to_left = false;
+        move_to_right = true;
+    }
 }
 
 void Goomba::update()
 {
-
+   if(move_to_right) this->coord_x =+ 5;
+   if(move_to_left) this->coord_x =- 5;
 }
+
