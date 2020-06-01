a3295526669586deed73c9d65e9a23f42c0c9f90
Nekosse <pinot.leo@gmail.com>
2020-06-01 19:00:18 +0200

    Correction 32 bit

--- Mario/Models/level.cpp
+++ Mario/Models/level.cpp
@@ -45,11 +45,11 @@ Level::Level()
 
             }
             if(clrCurrent.red() == 255 && clrCurrent.green()==242 &&clrCurrent.blue()==0){
-                qInfo() << "New Piece" ;
                 alive_entity_list->push_back(new Piece());
                 alive_entity_list->last()->setDisplay(true);
                 alive_entity_list->last()->setCoordX(col-10);
                 alive_entity_list->last()->setCoordY(row-10);
+                 alive_entity_list->last()->setState(0);
             }
             if(clrCurrent.red() == 34 && clrCurrent.green()==177 &&clrCurrent.blue()==76){
                 alive_entity_list->push_back(new Koopa());
