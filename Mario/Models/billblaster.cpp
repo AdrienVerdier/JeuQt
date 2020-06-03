6cd26f5934ff5b77f7bfd238544d1f13a6ba9afa
PierreSavary1 <pierre.savary1@gmail.com>
2020-06-03 16:01:12 +0200

    thwomp + bill update

--- Mario/Models/billblaster.cpp
+++ Mario/Models/billblaster.cpp
@@ -18,7 +18,7 @@ void billblaster::collision(Entity *entity, int position)
 
 void billblaster::update()
 {
-    if (cpt_frame == 30){
+    if (cpt_frame == 60){
         smoke * s = new smoke();
         level->get_alive_entity_list()->push_back(s);
         s->setDisplay(true);
@@ -26,6 +26,17 @@ void billblaster::update()
         if(level->getPlayer()->getDistanceOn_X(coord_x)<0)s->setCoordX(coord_x+55);
         s->setCoordY(coord_y);
         s->setState(0);
+
+        bool left = true;
+        if(level->getPlayer()->getDistanceOn_X(coord_x)<0) left = false;
+        bulletbill * b = new bulletbill(left);
+        level->get_alive_entity_list()->push_back(b);
+        b->setDisplay(true);
+        if(level->getPlayer()->getDistanceOn_X(coord_x)>0)b->setCoordX(coord_x-30);
+        if(level->getPlayer()->getDistanceOn_X(coord_x)<0)b->setCoordX(coord_x+55);
+        b->setCoordY(coord_y);
+        b->setState(0);
+
         cpt_frame = 0;
     }
     cpt_frame++;
--- Mario/Models/thwomp.cpp
+++ Mario/Models/thwomp.cpp
@@ -1,5 +1,6 @@
 #include "thwomp.h"
 #include "mario.h"
+#include "level.h"
 
 #include <typeinfo>
 #include <QDebug>
@@ -10,7 +11,7 @@ thwomp::thwomp()
 
 void thwomp::collision(Entity *entity, int position)
 {
-    if(position == 2 && typeid (Block).name() != typeid(*entity).name() && typeid (Mario).name() != typeid(*entity).name() && !getOn_block()){
+    if(position == 2 && typeid (Block).name() != typeid(*entity).name() && typeid (bulletbill).name() != typeid(*entity).name() && typeid (Mario).name() != typeid(*entity).name() && !getOn_block()){
         move_to_down = false;
         move_to_up = true;
         state=1;
@@ -25,31 +26,39 @@ void thwomp::collision(Entity *entity, int position)
 
 void thwomp::update()
 {
-
    if(getInitial_y()==-1) setInitial_y(this->coord_y);
-   if(move_to_up) this->coord_y -= 4;
-   if(!getOn_block())
+   if(level->getPlayer()->getDistanceOn_X(coord_x)>0 && level->getPlayer()->getDistanceOn_X(coord_x)<100 || level->getPlayer()->getDistanceOn_X(coord_x)<0 && level->getPlayer()->getDistanceOn_X(coord_x)>-200 || coord_y != getInitial_y())
    {
-       if(move_to_down) this->coord_y += 4;
-       if(this->coord_y <= initial_y)
+       if(move_to_up) this->coord_y -= 4;
+       if(!getOn_block())
        {
-           move_to_up = false;
-           move_to_down = true;
-           state=0;
+           if(move_to_down) {
+               state=0;
+               this->coord_y += 4;
+           }
+           if(this->coord_y <= initial_y)
+           {
+               move_to_up = false;
+               move_to_down = true;
+               state=0;
+           }
+       }
+       else{
+           setCompteur(getCompteur()+1);
+       }
+       if(getCompteur() == 50)
+       {
+           move_to_up = true;
+           state=1;
+       }
+       if(getCompteur() == 55)
+       {
+           setOn_block(false);
+           setCompteur(0);
        }
    }
-   else{
-       setCompteur(getCompteur()+1);
-   }
-   if(getCompteur() == 50)
-   {
-       move_to_up = true;
-       state=1;
-   }
-   if(getCompteur() == 55)
-   {
-       setOn_block(false);
-       setCompteur(0);
+    else{
+       state=2;
    }
 }
 
